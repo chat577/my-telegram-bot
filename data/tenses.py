ccd1c5509a99dbeac77223e3e07207b871aab502
123 <shematehnik55@gmail.com>
2025-10-21 20:41:41 +0600

    Create tenses.py

---
+++ data/tenses.py
@@ -0,0 +1,51 @@
+# Английские времена с примерами
+TENSES = {
+    "present_simple": {
+        "name": "Present Simple",
+        "russian_name": "Настоящее Простое",
+        "usage": "Постоянные действия, привычки, факты",
+        "structure": "Subject + V1/V1+s",
+        "examples": [
+            "I work every day - Я работаю каждый день",
+            "He works in an office - Он работает в офисе",
+            "We like music - Нам нравится музыка"
+        ],
+        "signal_words": ["always", "usually", "often", "every day", "sometimes"]
+    },
+    "present_continuous": {
+        "name": "Present Continuous",
+        "russian_name": "Настоящее Длительное",
+        "usage": "Действия в момент речи, временные ситуации",
+        "structure": "Subject + am/is/are + V-ing",
+        "examples": [
+            "I am reading now - Я сейчас читаю",
+            "She is watching TV - Она смотрит телевизор",
+            "They are playing football - Они играют в футбол"
+        ],
+        "signal_words": ["now", "at the moment", "currently", "today"]
+    },
+    "past_simple": {
+        "name": "Past Simple",
+        "russian_name": "Прошедшее Простое",
+        "usage": "Завершенные действия в прошлом",
+        "structure": "Subject + V2",
+        "examples": [
+            "I worked yesterday - Я работал вчера",
+            "She went to school - Она ходила в школу",
+            "We saw a movie - Мы смотрели фильм"
+        ],
+        "signal_words": ["yesterday", "last week", "ago", "in 2020"]
+    },
+    "future_simple": {
+        "name": "Future Simple",
+        "russian_name": "Будущее Простое",
+        "usage": "Спонтанные решения, предсказания, обещания",
+        "structure": "Subject + will + V1",
+        "examples": [
+            "I will help you - Я помогу тебе",
+            "It will rain tomorrow - Завтра будет дождь",
+            "We will travel - Мы будем путешествовать"
+        ],
+        "signal_words": ["tomorrow", "next week", "soon", "in the future"]
+    }
+}
