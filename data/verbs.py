172b74c2126a5d9958d21d129fcccb3acaab1d75
123 <shematehnik55@gmail.com>
2025-10-21 20:41:11 +0600

    Create verbs.py

---
+++ data/verbs.py
@@ -0,0 +1,27 @@
+# Неправильные глаголы
+IRREGULAR_VERBS = [
+    {"infinitive": "be", "past": "was/were", "participle": "been", "translation": "быть", "level": "A1"},
+    {"infinitive": "have", "past": "had", "participle": "had", "translation": "иметь", "level": "A1"},
+    {"infinitive": "do", "past": "did", "participle": "done", "translation": "делать", "level": "A1"},
+    {"infinitive": "go", "past": "went", "participle": "gone", "translation": "идти", "level": "A1"},
+    {"infinitive": "see", "past": "saw", "participle": "seen", "translation": "видеть", "level": "A1"},
+    {"infinitive": "come", "past": "came", "participle": "come", "translation": "приходить", "level": "A1"},
+    {"infinitive": "get", "past": "got", "participle": "got/gotten", "translation": "получать", "level": "A1"},
+    {"infinitive": "give", "past": "gave", "participle": "given", "translation": "давать", "level": "A1"},
+    {"infinitive": "take", "past": "took", "participle": "taken", "translation": "брать", "level": "A1"},
+    {"infinitive": "make", "past": "made", "participle": "made", "translation": "делать", "level": "A1"},
+]
+
+# Основные глаголы для начинающих
+BASIC_VERBS = [
+    {"english": "like", "russian": "нравиться", "example": "I like coffee"},
+    {"english": "want", "russian": "хотеть", "example": "I want water"},
+    {"english": "need", "russian": "нуждаться", "example": "I need help"},
+    {"english": "can", "russian": "мочь", "example": "I can swim"},
+    {"english": "know", "russian": "знать", "example": "I know this"},
+    {"english": "think", "russian": "думать", "example": "I think so"},
+    {"english": "say", "russian": "говорить", "example": "I say hello"},
+    {"english": "see", "russian": "видеть", "example": "I see you"},
+    {"english": "come", "russian": "приходить", "example": "I come home"},
+    {"english": "look", "russian": "смотреть", "example": "Look at me"},
+]
